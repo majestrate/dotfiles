843aede7211ec81bf0b8062ee40ec74479613798
jeff <ampernand@gmail.com>
2015-07-08 08:18:56 -0400

    add ftp.py

---
+++ ftp.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+rootdir = '/var/www'
+auth = DummyAuthorizer()
+with open('/root/ftpauth.txt') as f:
+    for line in f:
+        line = line.strip()
+        if line.startswith('#'):
+            continue
+        if ':' in line:
+            idx = line.index(':')
+            user, passwd = line[idx:], line[:1+idx]
+            auth.add_user(user, passwd, rootdir, perm="elradfmw")
+auth.add_anonymous(rootdir)
+handler = FTPHandler
+handler.authorizer = authorizer
+server = FTPServer(("[::]%tun0", 21), handler)
+server.serve_forever()
