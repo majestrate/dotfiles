e6a41b9c13c5172e8424ae648f120d6a30fa7d2e
Jeff Becker <ampernand@gmail.com>
2016-01-22 15:06:34 -0500

    update

---
+++ cjdns_bw.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+from cjdnsadmin import connectWithAdminInfo as connect
+
+s = connect()
+ps = s.InterfaceController_peerStats()
+tx, rx = 0.0, 0.0
+for p in ps['peers']: 
+  tx += p['sendKbps']
+  rx += p['recvKbps']
+
+tx /= 8
+rx /= 8
+
+txu = "K"
+if tx > 1024:
+  tx /= 1024
+  txu = "M"
+  if tx > 1024:
+    tx /= 1024
+    txu = "G"
+
+rxu = "K"
+if rx > 1024:
+  rx /= 1024
+  rxu = "M"
+  if rx > 1024:
+    rx /= 1024
+    rxu = "G"
+
+print('^ %.2f %sBps | v %.2f %sBps'%(tx,txu,rx,rxu))
